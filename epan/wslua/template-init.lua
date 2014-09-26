8be7a26e3a871205eea1148e6132a642ba3d7b50
Fred Klassen <fklassen@appneta.com>
2014-09-26 08:53:10 -0700

    disable lua to prevent warnings on PV appliances

--- epan/wslua/template-init.lua
+++ epan/wslua/template-init.lua
@@ -26,7 +26,7 @@
 -- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 -- Set disable_lua to true to disable Lua support.
-disable_lua = false
+disable_lua = true
 
 if disable_lua then
     return
